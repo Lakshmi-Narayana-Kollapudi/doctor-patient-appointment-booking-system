36a6ed8a0271147959ee5deb7987666c8ef4c086
Lakshmi-Narayana-Kollapudi <lakshminarayanakollapudi00@gmail.com>
2023-12-23 22:16:32 +0530

    after added secret key

--- src/__init__.py
+++ src/__init__.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
