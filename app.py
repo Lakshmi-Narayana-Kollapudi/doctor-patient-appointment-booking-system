672c075bb2340e03f31c5a1a37b155816ccdb3f2
Lakshmi-Narayana-Kollapudi <lakshminarayanakollapudi00@gmail.com>
2023-12-23 20:28:56 +0530

    after adding requirements.txt file

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 from src import app
 
-#Checks if the run.py file has executed directly and not imported
+# Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # Default to port 10000 if PORT is not set
+    app.run(debug=False, host='0.0.0.0', port=port)
