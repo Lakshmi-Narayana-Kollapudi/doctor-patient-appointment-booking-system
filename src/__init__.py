8c16ecfe47d6336833689a3c0ab4d4a056f15c20
Lakshmi-Narayana-Kollapudi <lakshminarayanakollapudi00@gmail.com>
2023-12-23 21:00:11 +0530

    after adding .env file

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,16 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+load_dotenv()
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
