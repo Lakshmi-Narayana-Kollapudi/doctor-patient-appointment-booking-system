fbd6b24d1b8b6f4f27c7dc249856a0250273e3fc
Lakshmi-Narayana-Kollapudi <lakshminarayanakollapudi00@gmail.com>
2024-01-03 20:38:46 +0530

    after changing debug mode False

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from src import app
 # Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # Default to port 10000 if PORT is not set
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
